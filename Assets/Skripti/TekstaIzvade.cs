fe482012a0503fecc3716e084f5c8955996f023b
leimanis <niks.leimanis3@gmail.com>
2022-02-16 14:15:00 +0200

    Istenotas darbibas ar tekstu

---
+++ Assets/Skripti/TekstaIzvade.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TekstaIzvade : MonoBehaviour {
+	//Uzglabās tekstu, kurš paņemts no input laika
+	public string teksts;
+	//Lauks no, kura es tekstu paņemšu
+	public GameObject ievadesLauks;
+	//Teksta objekts, kur attēlot mainīgo teksta saturu
+	public GameObject TekstaAttelosanasLauks;
+
+	public void darbibasArTekstu(){
+		teksts = ievadesLauks.GetComponent<Text> ().text;
+		TekstaAttelosanasLauks.GetComponent<Text> ().text = "Sveiks " + teksts.ToUpper()+"!";
+	}
+}
